e724b508596fa37fbbe017468789ddb6116c4374
suman11111111 <sumansingharoy1998@gmail.com>
2025-03-16 19:48:21 +0530

    16/03

--- IEEE3_2/ieee3_fa_2.m
+++ IEEE3_2/ieee3_fa_2.m
@@ -75,7 +75,7 @@ f=@(x)[(-x(1)-(Xd1-Xd_dash1)*x(8)+Efd1);
     x(2)-x(13)*cos(x(16))*cos(x(5))-x(13)*sin(x(16))*sin(x(5))-Rs2*x(11)-Xd_dash2*x(9);
     x(8)*x(12)*sin(-x(15))+x(10)*x(12)*cos(-x(15))+P1-B11*x(12)*x(12)*sin(x(15)-x(15))-B12*x(12)*x(13)*sin(x(15)-x(16))-B13*x(12)*x(14)*sin(x(15)-x(17));
     x(9)*x(13)*sin(x(5)-x(16))+x(11)*x(13)*cos(x(5)-x(16))+P2-B21*x(13)*x(12)*sin(x(16)-x(15))-B22*x(13)*x(13)*sin(x(16)-x(16))-B23*x(13)*x(14)*sin(x(16)-x(17));
-    P3-B31*x(14)*x(12)*sin(x(17)-x(15))-B32*x(14)*x(13)*sin(x(17)-x(16))-B33*x(14)*x(14)*sin(x(17)-x(1));
+    P3-B31*x(14)*x(12)*sin(x(17)-x(15))-B32*x(14)*x(13)*sin(x(17)-x(16))-B33*x(14)*x(14)*sin(x(17)-x(17));
     x(8)*x(12)*cos(-x(15))-x(10)*x(12)*sin(-x(15))+Q1-B11*x(12)*x(12)*cos(x(15)-x(15))-B12*x(12)*x(13)*cos(x(15)-x(16))-B13*x(12)*x(14)*cos(x(15)-x(17));
     x(9)*x(13)*cos(x(5)-x(16))-x(11)*x(12)*sin(x(5)-x(16))+Q2-B21*x(13)*x(12)*cos(x(16)-x(15))-B22*x(13)*x(13)*cos(x(16)-x(16))-B23*x(13)*x(14)*cos(x(16)-x(17));
     Q3-B31*x(14)*x(12)*cos(x(17)-x(15))-B32*x(14)*x(13)*cos(x(17)-x(16))-B33*x(14)*x(14)*cos(x(17)-x(17));
@@ -91,11 +91,15 @@ x0=[-0.0477;-0.5021;0.0298;-0.0964;-0.4354;1;1;0.466;-0.1046;-0.05189;-0.3966;1;
 options = optimset('Algorithm', 'levenberg-marquardt','TolFun', 1e-6, 'TolX', 1e-6,'MaxIter', 10000000, 'MaxFunEvals', 1000000);
 [xeq,fval,flag,out]=fsolve(f,x0,options);
 
+%Sensor Spoofing Attack
+xeq(16)=-0.9175;
+
+
 %Defining of Symbolic Variables for each bus
 syms omega_s
-syms Eq1 Ed1 omega1 Id1 Iq1 P1 Q1 theta1 V1 Efd1 H1 TM1 D1 Tdo1 Tqo1 Rs1 Xq1 Xd1 Xd_dash1 Xq1 Xq_dash1 B11 B12 B13
-syms Eq2 Ed2 delt2 omega2 Id2 Iq2 P2 Q2 theta2 V2 Efd2 H2 TM2 D2 Tdo2 Tqo2 Rs2 Xq2 Xd2 Xd_dash2 Xq2 Xq_dash2 B21 B22 B23
-syms P3 Q3 theta3 V3 B31 B32 B33
+syms Eq1 Ed1 omega1 Id1 Iq1 theta1 V1 Efd1 H1 TM1 D1 Tdo1 Tqo1 Rs1 Xq1 Xd1 Xd_dash1 Xq1 Xq_dash1 B11 B12 B13
+syms Eq2 Ed2 delt2 omega2 Id2 Iq2 theta2 V2 Efd2 H2 TM2 D2 Tdo2 Tqo2 Rs2 Xq2 Xd2 Xd_dash2 Xq2 Xq_dash2 B21 B22 B23
+syms theta3 V3 B31 B32 B33
 
 
 %Differential Equation for 1st bus
@@ -104,13 +108,13 @@ f1(2)=(-Ed1-(Xq1-Xq_dash1)*Iq1);
 f1(3)=TM1-Ed1*Id1-Eq1*Iq1-(Xq_dash1-Xd_dash1)*Id1*Iq1-D1*omega1;
 
 %Algebraic Equation for 1st bus
-f1(4)=Ed1-V1*cos(theta1)*sin(delt1)+V1*sin(theta1)*cos(delt1)-Rs1*Id1+Xq_dash1*Iq1;
-f1(5)=Eq1-V1*cos(theta1)*cos(delt1)-V1*sin(theta1)*sin(delt1)-Rs1*Iq1-Xd_dash1*Id1;
-f1(6)=Id1*V1*sin(delt1-theta1)+Iq1*V1*cos(delt1-theta1)+P1-B11*V1*V1*sin(theta1-theta1)-B12*V1*V2*sin(theta1-theta2)-B13*V1*V3*sin(theta1-theta3);
-f1(7)=Id1*V1*cos(delt1-theta1)-Iq1*V1*sin(delt1-theta1)+Q1-B11*V1*V1*cos(theta1-theta1)-B12*V1*V2*cos(theta1-theta2)-B13*V1*V3*cos(theta1-theta3);
+f1(4)=Ed1+V1*sin(theta1)-Rs1*Id1+Xq_dash1*Iq1;
+f1(5)=Eq1-V1*cos(theta1)-Rs1*Iq1-Xd_dash1*Id1;
+f1(6)=Id1*V1*sin(-theta1)+Iq1*V1*cos(-theta1)+P1-B11*V1*V1*sin(theta1-theta1)-B12*V1*V2*sin(theta1-theta2)-B13*V1*V3*sin(theta1-theta3);
+f1(7)=Id1*V1*cos(-theta1)-Iq1*V1*sin(-theta1)+Q1-B11*V1*V1*cos(theta1-theta1)-B12*V1*V2*cos(theta1-theta2)-B13*V1*V3*cos(theta1-theta3);
 
 %States for 1st bus
-x1=[Eq1 Ed1 delt1 omega1 Iq1 Id1 V1 theta1];
+x1=[Eq1 Ed1 omega1 Iq1 Id1 V1 theta1];
 
 %Input States for 1st bus
 u1=[Efd1 TM1];
@@ -118,7 +122,7 @@ u1=[Efd1 TM1];
 %Differential Equation for 2nd bus
 f2(1)=(-Eq2-(Xd2-Xd_dash2)*Id2+Efd2);
 f2(2)=(-Ed2-(Xq2-Xq_dash2)*Iq2);
-f2(3)=omega2;
+f2(3)=omega2-omega1;
 f2(4)=TM2-Ed2*Id2-Eq2*Iq2-(Xq_dash2-Xd_dash2)*Id2*Iq2-D2*omega2;
 
 %Algebraic Equation for 2nd bus
@@ -141,11 +145,11 @@ f3(2)=Q3-B31*V3*V1*cos(theta3-theta1)-B32*V3*V2*cos(theta3-theta2)-B33*V3*V3*cos
 x3=[V3 theta3];
 
 %Drifts
-fd=[f1(1),f2(1),f1(2),f2(2),f1(3),f2(3),f1(4),f2(4)];
-xd=[x1(1),x2(1),x1(2),x2(2),x1(3),x2(3),x1(4),x2(4)];
+fd=[f1(1),f2(1),f1(2),f2(2),f2(3),f1(3),f2(4)];
+xd=[x1(1),x2(1),x1(2),x2(2),x2(3),x1(3),x2(4)];
 
-fa=[f1(5),f2(5),f1(6),f2(6),f1(7),f2(7),f3(1),f1(8),f2(8),f3(2)];
-xa=[x1(5),x2(5),x1(6),x2(6),x1(7),x2(7),x3(1),x1(8),x2(8),x3(2)];
+fa=[f1(4),f2(5),f1(5),f2(6),f1(6),f2(7),f3(1),f1(7),f2(8),f3(2)];
+xa=[x1(4),x2(5),x1(5),x2(6),x1(6),x2(7),x3(1),x1(7),x2(8),x3(2)];
 
 u=[u1(1),u2(1),u1(2),u2(2)];
 
@@ -162,19 +166,81 @@ A=[A11,A12;A21,A22];
 B=[B1;B2];
 
 %Computation of E
-E=zeros(18,18);
+E=zeros(17,17);
 E=sym(E);
 E(1,1)=Tdo1;
 E(2,2)=Tdo2;
 E(3,3)=Tqo1;
 E(4,4)=Tqo2;
 E(5,5)=1;
-E(6,6)=1;
-E(7,7)=(2*H1)/omega_s;
-E(8,8)=(2*H2)/omega_s;
+E(6,6)=(2*H1)/omega_s;
+E(7,7)=(2*H2)/omega_s;
 
-%B Matrix
-B = [1,0,0,0;0,1,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,1,0;0,0,0,1;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0;0,0,0,0];
+Hs=[H1 H2];
+H=[6.4 3.01];
+
+Eqs=[Eq1 Eq2];
+Eq=[xeq(1) xeq(2)];
+
+Eds=[Ed1 Ed2];
+Ed=[xeq(3) xeq(4)];
+
+Vs=[V1 V2 V3];
+V=[xeq(12) xeq(13) xeq(14)];
+
+delts=delt2;
+delt=[xeq(5)];
+
+omegas=[omega1 omega2];
+omega=[xeq(6) xeq(7)];
+
+Ds=[D1 D2];
+D=[0.3 0.3];
+
+Ids=[Id1 Id2];
+Id=[xeq(8) xeq(9)];
+
+Iqs=[Iq1 Iq2];
+Iq=[xeq(10) xeq(11)];
+
+Rss=[Rs1 Rs2];
+Rs=[0 0];
+
+Xds=[Xd1 Xd2];
+Xd=[0.8958 1.3125];
+
+Xqs=[Xq1 Xq2];
+Xq=[0.8645 1.2578];
+
+Xd_dashs=[Xd_dash1 Xd_dash2];
+Xd_dash=[0.1198 0.1813];
+
+Xq_dashs=[Xq_dash1 Xq_dash2];
+Xq_dash=[0.1969 0.25];
+
+Tdos=[Tdo1 Tdo2];
+Tdo=[6 5.89];
+
+Tqos=[Tqo1 Tqo2];
+Tqo=[0.535 0.6];
+
+thetas=[theta1 theta2 theta3];
+theta=[xeq(15) xeq(16) xeq(17)];
+
+B1xs=[B11 B12 B13];
+B1x=[-0.25 0.1 0.15];
+
+B2xs=[B22 B21 B23];
+B2x=[0 0 0];
+
+B3s=[B33 B31 B32];
+B3=[-0.25 0.15 0.1];
+
+E=subs(E,[Tdos Tqos Hs omega_s],[Tdo Tqo H 1]);
+E=double(E);
+A=subs(A,[Eqs Eds Vs delts omegas Ds Ids Iqs Rss Xds Xqs Xd_dashs Xq_dashs Tdos Tqos thetas B1xs B2xs B3s],[Eq Ed V delt omega D Id Iq Rs Xd Xq Xd_dash Xq_dash Tdo Tqo theta B1x B2x B3]);
+A=double(A);
+B=double(B);
 
 pol=eig(A,E);
 s_pol=size(pol);
@@ -195,3 +261,11 @@ im_cont=(rank([E A B;zeros(17,17) E zeros(17,4)])==rank(E)+rank([E A B]));
 adm=(rank([s*E-A B E*xeq])==rank([s*E-A B]));
 adm_ar=(rank([E A B])==rank([s*E-A B]));
 
+tspan=[0 600];
+options=odeset('Mass',E,'MStateDependence','none','MassSingular','yes');
+[t,x]=ode23t(@(t,x) des(A,B,x),tspan,xeq,options);
+
+function dae=des(A,B,x)
+    u=[1;1;1.63;0.85];
+    dae=A*x+B*u;
+end
